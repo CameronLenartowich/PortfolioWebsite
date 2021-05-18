b0533013791e0567287c039b12d6acfba212b779
CameronLenartowich <camtl66@gmail.com>
2021-05-17 23:47:05 -0400

    new version initially deployed

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
+import React from 'react'
 import '../styles/App.css';
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUKCET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
+        <h1>Under Maintenance</h1>
       </header>
     </div>
   );
