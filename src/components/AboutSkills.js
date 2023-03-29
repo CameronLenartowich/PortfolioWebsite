cf626be6aea337b28eb9cc7eeac766f17529b3ea
CameronLenartowich <camtl66@gmail.com>
2023-03-28 21:45:52 -0400

    skills

--- src/components/AboutSkills.js
+++ src/components/AboutSkills.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   titleh3: {
-    fontSize: '1.5em',
-    textAlign: 'left'
-  }
+    fontSize: "1.5em",
+    textAlign: "left",
+  },
 }));
 
 export default function AboutSkills() {
@@ -21,149 +21,138 @@ export default function AboutSkills() {
 
   return (
     <div className={classes.root}>
-          <Grid container spacing={3}>
+      <Grid container spacing={3}>
+        <Grid item sm={6} xs={12}>
+          <Typography variant="h3" gutterBottom className={classes.titleh3}>
+            Front End
+          </Typography>
+          <List component="nav" aria-label="secondary">
+            <ListItem>
+              <ListItemText primary="React" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Svelte" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Tailwind" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Material-UI" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Typescript" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Javascript" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="CSS" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="HTML" />
+            </ListItem>
+          </List>
+        </Grid>
 
-            <Grid item sm={6} xs={12}>
-              <Typography
-                variant="h3" 
-                gutterBottom
-                className={classes.titleh3}
-              >
-                Front End
-              </Typography>
-              <List component="nav" aria-label="secondary">
-                <ListItem>
-                  <ListItemText primary="HTML" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="CSS" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Bootstrap" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Material-UI" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="React.js" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Javascript" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Redux" />
-                </ListItem>
-              </List>
-            </Grid>
+        <Grid item sm={6} xs={12}>
+          <Typography variant="h3" gutterBottom className={classes.titleh3}>
+            Backend End
+          </Typography>
+          <List component="nav" aria-label="secondary">
+            <ListItem>
+              <ListItemText primary="C#" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Node.js" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="REST" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="GraphQL" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="SQL" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="NoSQL" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="ORM's" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Microsoft SQL Server" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="PostgreSQL" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Google Firebase" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="MongoDB" />
+            </ListItem>
+          </List>
+        </Grid>
 
-            <Grid item sm={6} xs={12}>
-              <Typography
-                variant="h3" 
-                gutterBottom
-                className={classes.titleh3}
-              >
-                Backend End
-              </Typography>
-              <List component="nav" aria-label="secondary">
-                <ListItem>
-                  <ListItemText primary=".Net Core C#" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Node.js" />
-                </ListItem>                
-                <ListItem>
-                  <ListItemText primary="API's" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="REST" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="GraphQL" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="IIS" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Heroku" />
-                </ListItem>
-              </List>
-            </Grid>
-          
+        <Grid item sm={6} xs={12}>
+          <Typography variant="h3" gutterBottom className={classes.titleh3}>
+            Development Workflow
+          </Typography>
+          <List component="nav" aria-label="secondary">
+            <ListItem>
+              <ListItemText primary="Git Version Control" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Unit and Integration Testing" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Authentication and Authorization" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Algorithms" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Data Structures" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Heroku" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Vercel" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="AWS" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Google Cloud Platform" />
+            </ListItem>
+          </List>
+        </Grid>
 
-          
-            <Grid item sm={6} xs={12}>
-              <Typography
-                variant="h3" 
-                gutterBottom
-                className={classes.titleh3}
-              >
-                Database Tools
-              </Typography>
-              <List component="nav" aria-label="secondary">
-                <ListItem>
-                  <ListItemText primary="SQL" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="NoSQL" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="ORM's" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Entity Framework" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Sequelize" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Microsoft SQL Server" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="PostgreSQL" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Google Firebase" />
-                </ListItem>
-              </List>
-            </Grid>
-
-            <Grid item sm={6} xs={12}>
-              <Typography
-                variant="h3" 
-                gutterBottom
-                className={classes.titleh3}
-              >
-                Development Workflow
-              </Typography>
-              <List component="nav" aria-label="secondary">
-                <ListItem>
-                  <ListItemText primary="Git Version Control" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Bash Script Command Line" />
-                </ListItem>                
-                <ListItem>
-                  <ListItemText primary="Test Driven Development" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Jest and Enzyme" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Jasmine" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Authentication and Authorization" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Algorithms" />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary="Data Structures" />
-                </ListItem>
-              </List>
-            </Grid>
-
-          </Grid>
+        <Grid item sm={6} xs={12}>
+          <Typography variant="h3" gutterBottom className={classes.titleh3}>
+            Ethereum
+          </Typography>
+          <List component="nav" aria-label="secondary">
+            <ListItem>
+              <ListItemText primary="Solidity" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Ethers" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Hardhat" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Login XYZ" />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="TheGraph" />
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
     </div>
   );
 }
